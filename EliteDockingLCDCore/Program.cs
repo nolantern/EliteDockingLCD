201c5cfe063e892f09261f7affed45aa61b7ccb3
nolantern <f.kraemer@posteo.de>
2021-01-09 13:18:14 +0100

    made app single instance using mutex pattern

--- EliteDockingLCDCore/Program.cs
+++ EliteDockingLCDCore/Program.cs
@@ -12,31 +12,55 @@ using Valsom.Logging.PrettyConsole;
 using Valsom.Logging.PrettyConsole.Formats;
 using Valsom.Logging.PrettyConsole.Themes;
 
-// Build the host for dependency injection
-var host = Host.CreateDefaultBuilder()
-    
-    .ConfigureLogging((context, logger) =>
-    {
-        logger.ClearProviders();
-        logger.SetMinimumLevel(LogLevel.Error);
-        //logger.AddPrettyConsole(ConsoleFormats.Default, ConsoleThemes.OneDarkPro);
-    })
-    
+using System.Threading;
+using System;
 
-    .ConfigureServices((context, services) =>
+
+// Allow only one App instance using Mutex
+using (var mutex = new Mutex(false, "nolantern.elitedockinglcd"))
+{
+    bool isAnotherInstanceOpen = !mutex.WaitOne(TimeSpan.Zero);
+    if (isAnotherInstanceOpen)
     {
-        services.AddEliteAPI(configuration =>
+        // Do something when another instance is already running and return.
+        return;
+    }
+    // App entry point (after Mutex pattern)
+
+    try
+    {
+        // Build the host for dependency injection
+        var host = Host.CreateDefaultBuilder()
+
+        .ConfigureLogging((context, logger) =>
         {
-            configuration.AddEventModule<LandingPadModule>();
-            //configuration.AddEventModule<ChatModule>();
-            //configuration.AddEventModule<StartypeModule>();
-        });
-    })
+            logger.ClearProviders();
+            logger.SetMinimumLevel(LogLevel.Error);
+            //logger.AddPrettyConsole(ConsoleFormats.Default, ConsoleThemes.OneDarkPro);
+        })
 
-    .Build();
 
-var core = ActivatorUtilities.CreateInstance<Core>(host.Services);
+        .ConfigureServices((context, services) =>
+        {
+            services.AddEliteAPI(configuration =>
+            {
+                configuration.AddEventModule<LandingPadModule>();
+                //configuration.AddEventModule<ChatModule>();
+                //configuration.AddEventModule<StartypeModule>();
+            });
+        })
 
-await core.Run();
+        .Build();
 
-await Task.Delay(-1);
+        var core = ActivatorUtilities.CreateInstance<Core>(host.Services);
+
+        await core.Run();
+
+        await Task.Delay(-1);
+    }
+
+    finally
+    {
+        mutex.ReleaseMutex();
+    }
+}
